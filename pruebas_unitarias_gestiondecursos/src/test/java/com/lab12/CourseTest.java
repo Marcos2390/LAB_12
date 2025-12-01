e043c90238b1f7e48d734d6b32fff27d7c8ea2d7
Marcos Silva <marcosdamian19902015@gmail.com>
2025-11-30 21:39:40 -0300

    Actualizar CourseTest con los últimos cambios

--- pruebas_unitarias_gestiondecursos/src/test/java/com/lab12/CourseTest.java
+++ pruebas_unitarias_gestiondecursos/src/test/java/com/lab12/CourseTest.java
@@ -1,8 +1,8 @@
 package com.lab12;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
 
 public class CourseTest {
     @Test
@@ -17,36 +17,4 @@ public class CourseTest {
         assertEquals(expected, c.showInformation());
     }
 
-    class Course {
-        private String title;
-        private int duration;
-        private String professor;
-
-        public Course(String title, int duration, String professor) {
-            this.title = title;
-            this.duration = duration;
-            this.professor = professor;
-        }
-
-        public Course(String title, int duration) {
-            this(title, duration, null);
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public int getDuration() {
-            return duration;
-        }
-
-        public String getProfessor() {
-            return professor;
-        }
-
-        public String showInformation() {
-            String prof = (professor != null) ? professor : "N/A";
-            return "Curso: " + title + " - Duración: " + duration + " horas - Profesor: " + prof;
-        }
-    }
 }
